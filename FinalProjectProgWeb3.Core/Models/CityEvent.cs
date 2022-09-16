46844cc81b47d91e1bc517a3fe17a332fcf49cb6
Roberto Avelino <roberto.avelino97@gmail.com>
2022-09-16 14:51:50 -0400

    Adding the Models Adding the models for the classes used in the program

---
+++ FinalProjectProgWeb3.Core/Models/CityEvent.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FinalProjectProgWeb3.Core.Models
+{
+    public class CityEvent
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        private long ? IdEvent {  get;  set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime DateHourEvent { get; set; }
+        public string Local { get; set; }
+        public string Address { get; set; }
+        public decimal Price { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
---
+++ FinalProjectProgWeb3.Core/Models/EventReservation.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+
+namespace FinalProjectProgWeb3.Core.Models
+{
+    public class EventReservation
+    {
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        private long ? IdReservation { get; set; }
+        public long IdEvent { get; set; }
+        public string PersonName { get; set; }
+        public long Quantity { get; set; }
+
+    }
+}
